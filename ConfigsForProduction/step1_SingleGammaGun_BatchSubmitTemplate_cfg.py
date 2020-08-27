1627b87a361c9d6b54e3b8806091ac8c4e075880
saptaparna <saptaparna@gmail.com>
2020-08-27 09:55:29 -0500

    Config for production

---
+++ ConfigsForProduction/step1_SingleGammaGun_BatchSubmitTemplate_cfg.py
@@ -0,0 +1,182 @@
+# Auto generated configuration file
+# using: 
+# Revision: 1.19 
+# Source: /local/reps/CMSSW/CMSSW/Configuration/Applications/python/ConfigBuilder.py,v 
+# with command line options: Configuration/GenProduction/python/TSG-Phase2HLTTDRWinter20GS-00064-fragment.py --fileout file:TSG-Phase2HLTTDRWinter20GS-00064.root --mc --eventcontent RAWSIM --datatier GEN-SIM --conditions 110X_mcRun4_realistic_v3 --beamspot HLLHC14TeV --step GEN,SIM --nThreads 2 --geometry Extended2026D49 --era Phase2C9 --python_filename TSG-Phase2HLTTDRWinter20GS-00064_1_cfg.py --no_exec --customise Configuration/DataProcessing/Utils.addMonitoring --customise_commands process.source.numberEventsInLuminosityBlock=cms.untracked.uint32(52910) -n 3768
+import FWCore.ParameterSet.Config as cms
+
+from Configuration.Eras.Era_Phase2C9_cff import Phase2C9
+
+process = cms.Process('SIM',Phase2C9)
+
+# import of standard configurations
+process.load('Configuration.StandardSequences.Services_cff')
+process.load('SimGeneral.HepPDTESSource.pythiapdt_cfi')
+process.load('FWCore.MessageService.MessageLogger_cfi')
+process.load('Configuration.EventContent.EventContent_cff')
+process.load('SimGeneral.MixingModule.mixNoPU_cfi')
+process.load('Configuration.Geometry.GeometryExtended2026D49Reco_cff')
+process.load('Configuration.Geometry.GeometryExtended2026D49_cff')
+process.load('Configuration.StandardSequences.MagneticField_cff')
+process.load('Configuration.StandardSequences.Generator_cff')
+#process.load('IOMC.EventVertexGenerators.VtxSmearedHLLHC14TeV_cfi')
+process.load('Configuration.StandardSequences.VtxSmearedNoSmear_cff')
+process.load('GeneratorInterface.Core.genFilterSummary_cff')
+process.load('Configuration.StandardSequences.SimIdeal_cff')
+process.load('Configuration.StandardSequences.EndOfProcess_cff')
+process.load('Configuration.StandardSequences.FrontierConditions_GlobalTag_cff')
+
+
+process.maxEvents = cms.untracked.PSet(
+    input = cms.untracked.int32(NEVENT),
+    output = cms.optional.untracked.allowed(cms.int32,cms.PSet)
+)
+
+# random seeds
+process.RandomNumberGeneratorService.generator.initialSeed = cms.untracked.uint32(NPART+NEVENT)
+process.RandomNumberGeneratorService.VtxSmeared.initialSeed = cms.untracked.uint32(NPART+NEVENT)
+
+
+
+# Input source
+process.source = cms.Source("EmptySource")
+
+process.source.firstLuminosityBlock = cms.untracked.uint32(NPART)
+
+process.options = cms.untracked.PSet(
+    FailPath = cms.untracked.vstring(),
+    IgnoreCompletely = cms.untracked.vstring(),
+    Rethrow = cms.untracked.vstring(),
+    SkipEvent = cms.untracked.vstring(),
+    allowUnscheduled = cms.obsolete.untracked.bool,
+    canDeleteEarly = cms.untracked.vstring(),
+    emptyRunLumiMode = cms.obsolete.untracked.string,
+    eventSetup = cms.untracked.PSet(
+        forceNumberOfConcurrentIOVs = cms.untracked.PSet(
+
+        ),
+        numberOfConcurrentIOVs = cms.untracked.uint32(1)
+    ),
+    fileMode = cms.untracked.string('FULLMERGE'),
+    forceEventSetupCacheClearOnNewRun = cms.untracked.bool(False),
+    makeTriggerResults = cms.obsolete.untracked.bool,
+    numberOfConcurrentLuminosityBlocks = cms.untracked.uint32(1),
+    numberOfConcurrentRuns = cms.untracked.uint32(1),
+    numberOfStreams = cms.untracked.uint32(0),
+    numberOfThreads = cms.untracked.uint32(1),
+    printDependencies = cms.untracked.bool(False),
+    sizeOfStackForThreadsInKB = cms.optional.untracked.uint32,
+    throwIfIllegalParameter = cms.untracked.bool(True),
+    wantSummary = cms.untracked.bool(False)
+)
+
+# Production Info
+process.configurationMetadata = cms.untracked.PSet(
+    annotation = cms.untracked.string('Configuration/GenProduction/python/PPD-PhaseIIMTDTDRAutumn18GS-00006-fragment.py nevts:1'),
+    name = cms.untracked.string('Applications'),
+    version = cms.untracked.string('$Revision: 1.19 $')
+)
+
+# Output definition
+
+process.RAWSIMoutput = cms.OutputModule("PoolOutputModule",
+    SelectEvents = cms.untracked.PSet(
+        SelectEvents = cms.vstring('generation_step')
+    ),
+    compressionAlgorithm = cms.untracked.string('LZMA'),
+    compressionLevel = cms.untracked.int32(1),
+    dataset = cms.untracked.PSet(
+        dataTier = cms.untracked.string('GEN-SIM'),
+        filterName = cms.untracked.string('')
+    ),
+    eventAutoFlushCompressedSize = cms.untracked.int32(20971520),
+    fileName = cms.untracked.string('file:step1_Gamma_EEnergy_nNEVENT_partNPART.root'),
+    outputCommands = process.RAWSIMEventContent.outputCommands,
+    splitLevel = cms.untracked.int32(0)
+)
+
+# Additional output definition
+
+# Other statements
+process.genstepfilter.triggerConditions=cms.vstring("generation_step")
+from Configuration.AlCa.GlobalTag import GlobalTag
+process.GlobalTag = GlobalTag(process.GlobalTag, '110X_mcRun4_realistic_v3', '')
+
+process.generator = cms.EDProducer("CloseByParticleGunProducer",
+    PGunParameters = cms.PSet(PartID = cms.vint32(22), # Since I'm shooting antiparticles, I'm putting gamma and K0 twice in the vector
+        EnMin = cms.double(Energy),
+        EnMax = cms.double(Energy),
+        RMin = cms.double(40),
+        RMax = cms.double(140),
+        ZMin = cms.double(320),
+        ZMax = cms.double(410),
+        Delta = cms.double(0),
+        Pointing = cms.bool(True),
+        Overlapping = cms.bool(False),
+        RandomShoot = cms.bool(False),
+        NParticles = cms.int32(1),
+        MaxEta = cms.double(2.7),
+        MinEta = cms.double(1.7),
+        MaxPhi = cms.double(3.14159265359),
+        MinPhi = cms.double(-3.14159265359)
+    ),
+    Verbosity = cms.untracked.int32(0),
+    firstRun = cms.untracked.uint32(NPART),
+    AddAntiParticle = cms.bool(False),
+    psethack = cms.string('Random particles in front of HGCAL')
+)
+
+"""
+
+process.generator = cms.EDProducer("FlatRandomPtGunProducer",
+    AddAntiParticle = cms.bool(False),
+    PGunParameters = cms.PSet(
+        PartID = cms.vint32(22),
+        MinPhi = cms.double(0.0),
+        MaxPhi = cms.double(0.0),
+        MinPt = cms.double(E),
+        MaxPt = cms.double(E),
+        MinEta = cms.double(1.79),
+        MaxEta = cms.double(1.81)
+    ),
+    Verbosity = cms.untracked.int32(0),
+    psethack = cms.string('multiple particles predefined pT/E eta 1p479 to 3')
+)
+"""
+process.generation_step = cms.Path(process.pgen)
+process.simulation_step = cms.Path(process.psim)
+process.genfiltersummary_step = cms.EndPath(process.genFilterSummary)
+process.endjob_step = cms.EndPath(process.endOfProcess)
+process.RAWSIMoutput_step = cms.EndPath(process.RAWSIMoutput)
+
+
+# Schedule definition
+process.schedule = cms.Schedule(process.generation_step,process.genfiltersummary_step,process.simulation_step,process.endjob_step,process.RAWSIMoutput_step)
+from PhysicsTools.PatAlgos.tools.helpers import associatePatAlgosToolsTask
+associatePatAlgosToolsTask(process)
+
+#Setup FWK for multithreaded
+process.options.numberOfThreads=cms.untracked.uint32(1)
+process.options.numberOfStreams=cms.untracked.uint32(0)
+process.options.numberOfConcurrentLuminosityBlocks=cms.untracked.uint32(1)
+# filter all path with the production filter sequence
+for path in process.paths:
+	#getattr(process,path).insert(0, process.ProductionFilterSequence)
+        getattr(process,path).insert(0, process.generator)
+ 
+# customisation of the process.
+
+# Automatic addition of the customisation function from Configuration.DataProcessing.Utils
+from Configuration.DataProcessing.Utils import addMonitoring 
+
+#call to customisation function addMonitoring imported from Configuration.DataProcessing.Utils
+process = addMonitoring(process)
+
+# End of customisation functions
+
+# Customisation from command line
+
+# Add early deletion of temporary data products to reduce peak memory need
+from Configuration.StandardSequences.earlyDeleteSettings_cff import customiseEarlyDelete
+process = customiseEarlyDelete(process)
+# End adding early deletion
